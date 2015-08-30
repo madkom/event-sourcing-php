a0f111bf76afc14784037a6f9b0cd61be9efd105
dgafka <dariss6666@gmail.com>
2015-08-30 15:57:19 +0200

    Added infrastructure layer

---
+++ Client/src/Infrastructure/UUIDGenerator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Dgafka\ES\Client\Infrastructure;
+
+use Rhumsaa\Uuid\Uuid;
+
+/**
+ * Class UUIDGenerator
+ *
+ * @package Dgafka\ES\Client\Infrastructure
+ * @author Dariusz Gafka <dgafka.mail@gmail.com>
+ */
+class UUIDGenerator implements \Dgafka\ES\Client\Application\Service\UUIDGenerator
+{
+
+	/**
+	 * @return string
+	 */
+	public function generateUUID()
+	{
+		return Uuid::uuid1();
+	}
+
+}

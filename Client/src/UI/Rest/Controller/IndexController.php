cec8aeb162d76dadf3bf6cd1416b3c72da0e9cb2
Darek Gafka <d.gafka@madkom.pl>
2015-08-31 15:42:48 +0200

    Added phalcon2 to project

---
+++ Client/src/UI/Rest/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Dgafka\ES\Client\UI\Rest\Controller;
+
+use Phalcon\Mvc\Controller;
+
+/**
+ * Class IndexController
+ * @package Dgafka\ES\Client\UI\Rest\Controller
+ * @author  Dariusz Gafka <d.gafka@madkom.pl>
+ */
+class IndexController extends Controller
+{
+
+    public function indexAction()
+    {
+       echo "yes";
+    }
+
+}
--- Client/web/index.php
+++ Client/web/index.php
@@ -1,3 +1,51 @@
 <?php
 
-echo "works";
+require('../vendor/autoload.php');
+
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+
+try {
+
+    // Create a DI
+    $di = new FactoryDefault();
+
+    // Setup the view component
+    $di->set('view', function () {
+        $view = new View();
+        $view->setContent('works');
+        return $view;
+    });
+
+    $di->set('url', function () {
+        $url = new UrlProvider();
+        $url->setBaseUri('/');
+        return $url;
+    });
+
+    $di->set('dispatcher', function(){
+        $dispatcher = new \Phalcon\Mvc\Dispatcher();
+        $dispatcher->setDefaultNamespace("Dgafka\\ES\\Client\\UI\\Rest\\Controller");
+        return $dispatcher;
+    });
+
+    $router = new \Phalcon\Mvc\Router();
+    $router->add(
+        '/test',
+        [
+            'controller' => 'IndexController',
+            'action'     => 'action'
+        ]
+    );
+    $router->handle();
+
+    // Handle the request
+    $application = new Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch (\Exception $e) {
+    echo "PhalconException: ", $e->getMessage();
+}
